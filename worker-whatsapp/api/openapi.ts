d0cc4f87c3a1617a0413bb87915aeb94810925e7
drexa1 <drexa1@hotmail.com>
2026-09-18 18:03:32 +0200

    Whatsapp endpoints

--- worker-whatsapp/api/openapi.ts
+++ worker-whatsapp/api/openapi.ts
@@ -58,23 +58,24 @@ export const getOpenApiSpec = () => ({
 });
 
 export const swaggerHtml = `
-	<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<title>API docs</title>
-			<link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css"/>
-		</head>
-		<body>
-			<div id="swagger-ui"></div>
-			<script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
-			<script>
-				// noinspection JSUnresolvedVariable
-				SwaggerUIBundle({
-					url: "/openapi.json",
-					dom_id: "#swagger-ui",
-					// docExpansion: "none"
-				});
-			</script>
-		</body>
-	</html>
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <title>API docs</title>
+            <link rel="icon" type="image/png" href="https://upload.wikimedia.org/wikipedia/commons/5/5e/WhatsApp_icon.png"/>
+            <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css"/>
+        </head>
+        <body>
+            <div id="swagger-ui"></div>
+            <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+            <script>
+                // noinspection JSUnresolvedVariable
+                SwaggerUIBundle({
+                    url: "/openapi.json",
+                    dom_id: "#swagger-ui",
+                    // docExpansion: "none"
+                });
+            </script>
+        </body>
+    </html>
 `;
