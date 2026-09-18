4352b229699586195d4b721286b1f9362038b0eb
drexa1 <drexa1@hotmail.com>
2026-09-18 18:42:24 +0200

    Whatsapp endpoints

--- worker-whatsapp/api/openapi.ts
+++ worker-whatsapp/api/openapi.ts
@@ -61,7 +61,7 @@ export const swaggerHtml = `
     <!DOCTYPE html>
     <html lang="en">
         <head>
-            <title>ksefbot-whatsapp-api]</title>
+            <title>ksefbot-whatsapp-api</title>
             <link rel="icon" type="image/png" href="https://static.vecteezy.com/system/resources/previews/016/716/480/non_2x/whatsapp-icon-free-png.png"/>
             <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css"/>
         </head>
