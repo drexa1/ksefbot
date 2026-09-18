7be166c9a41f844b98fc41ca06f93e2c21408d43
drexa1 <drexa1@hotmail.com>
2026-09-18 13:59:41 +0200

    Whatsapp endpoints

--- worker-whatsapp/api/openapi.ts
+++ worker-whatsapp/api/openapi.ts
@@ -1,7 +1,7 @@
 export const getOpenApiSpec = () => ({
     openapi: "3.0.0",
     info: {
-        title: "WhatsApp API",
+        title: "Whatsapp API",
         version: "1.0.0"
     },
     components: {
